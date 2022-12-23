1c6fa62b4504a146df733ae6bf4c84249c7d3412
erguzel <erguzel@itu.edu.tr>
2022-12-23 18:25:59 +0100

    rm object_from_module

--- src/exchelp/exception_helper.py
+++ src/exchelp/exception_helper.py
@@ -348,27 +348,7 @@ def dictionarize_data(data)->dict:
      return data
   except Exception as e:
     raise TypeError('dictionarize_data failed',e)
-    #
-    #
-    #
-def object_from_module(moduleName:str,objectName:str,subObjectName:str=None):
-    """Creates an instantiable meata-object of given module and object names combination
 
-    Args:
-        moduleName (str): name of the module
-        objectName (str): name of the class or function
-        subObjectName (str, optional): Name of the sub class or function. Defaults to None.
-
-    Returns:
-        _type_: a meta-object ready to be instantiated  """
-    try:
-        module = importlib.import_module(moduleName)
-        result_ = getattr(module, objectName)
-        if(subObjectName != None):
-            result_ = getattr(result_,subObjectName)
-        return result_
-    except Exception as e:
-        CoreException('object_from_module failed',e,dontThrow=True,logIt=True,shouldExit=True).adddata('locals',locals()).act()
 
 
     
